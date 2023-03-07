29e3cd1536aa69bf1b0b3df1709b4122a276ac74
Jonas Reycian B. Saraosos <jonasreycian@gmail.com>
2023-03-07 20:43:04 +0800

    Completed Dynamic Routes

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -32,11 +32,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                {title}
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
